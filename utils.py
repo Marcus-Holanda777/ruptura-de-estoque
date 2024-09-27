789dd9e9b4c67b78db8b7705d219ed9be1124ed2
Marcus Holanda <mvsh777@hotmail.com>
2024-09-27 18:13:12 -0300

    logs

--- utils.py
+++ utils.py
@@ -18,7 +18,7 @@ FILE_LOGS = logging.FileHandler(
 )
 
 logging.basicConfig(
-    level=logging.INFO, 
+    level=logging.WARNING,
     handlers=[FILE_LOGS],
     format='%(asctime)s - %(levelname)s - %(message)s', 
     datefmt='%d/%m/%Y %H:%M:%S'
